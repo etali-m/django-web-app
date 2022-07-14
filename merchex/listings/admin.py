5dcd27c0e3bc14cc632f24e52e2d6bffd60233c0
etali-m <etalijunior15@gmail.com>
2022-07-14 21:10:57 +0100

    admin site added

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre') #liste des gens des champs que nous souhaitons afficher
+
+admin.site.register(Band, BandAdmin)
+
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.core.validators import MaxValueValidator,MinValueValidator
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+            HIP_HOP = 'HH'
+            SYNTH_POP = 'SP'
+            ALTERNATIV_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+    genre = models.fields.CharField(max_length=50, choices=Genre.choices)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
-        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)] #permet de définir min et max pour le champ
     )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
